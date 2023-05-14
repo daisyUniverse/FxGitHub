f9e68ba848bb6f828831941089f24dfd72bbf2ff
Robin Universe <mail@robinuniver.se>
2023-05-14 16:57:12 -0500

    Improve caching, add logging, and fallback to whole document without args
    Made it so caching stores files named uniquely per repo/branch so it can avoid accidentally posting the contents of another identically named file. Also made it log a bit nicer. Also made it so if you give it a file without line arguments it'll just post the entire file

--- fxgithub.py
+++ fxgithub.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import subprocess
 import requests
-import random
+import logging
 import os.path
 
 # FXGithub
@@ -10,6 +10,8 @@ import os.path
 # 05 . 13 . 23
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG, filename='fxgithub.log', filemode='w', format='%(message)s')
 
 col = {
     "red"       : "\033[91m",
@@ -22,6 +24,8 @@ col = {
     "reset"     : "\033[0m"
 }
 
+logging.info(f"{col['green']} <3 FXGithub by Robin Universe started. {col['reset']}")
+
 @app.route('/')
 def default():
     return render_template('default.html', message="HELLO. WELCOME TO THE FXGITHUB TESTING INITIATIVE")
@@ -29,45 +33,58 @@ def default():
 @app.route('/<path:subpath>')
 def fxgithub(subpath):
     splitpath = subpath.split("/")
-    author = splitpath[0]
-    repo = splitpath[1]
-    action = splitpath[2]
-    branch = splitpath[3]
-    file = '/'.join(splitpath[4:])
+    try:
+        author  = splitpath[0]
+        repo    = splitpath[1]
+        action  = splitpath[2]
+        branch  = splitpath[3]
+        file    = '/'.join(splitpath[4:])
+    except:
+        logging.warning(f"{col['red']} [ !!! ERROR: ] {subpath} is not a valid github link {col['reset']}" )
+        return render_template('default.html', message="Please specify a github link with the format /author/repo/action/branch/file")
     
     if action != "blob":
         return render_template('default.html', message="Please specify a file with the blob action, e.g. /RobinUniverse/FXGithub/blob/main/twitfix.py")
-    
-    if "L" not in (list(request.args.keys())[0]):
-        return render_template('default.html', message="Please specify lines to show with the L parameter, e.g. /RobinUniverse/FXGithub/blob/main/twitfix.py?L1-10")
-    
-    lines       = (list(request.args.keys())[0]).replace("L","").split("-")
+
     rawfileurl  = ("https://raw.githubusercontent.com/" + author  + "/" + repo + "/" + branch + "/" + file)
-    codefile    = ("/home/robin/fxgithub/static/code/" + file.split("/")[-1])
-    imagefile   = ("/home/robin/fxgithub/static/img/" + file.split("/")[-1].split(".")[0] + "_" + "L" + lines[0] + "-" + lines[1] + ".png" )
-    githuburl   = ("https://github.com/" + author + "/" + repo + "/" + action + "/" + branch + "/" + file + "#L" + lines[0] + "-" + lines[1])
+    codefile    = ("/home/robin/fxgithub/static/code/" + author + "_" + repo + "_" + branch + "_" + file.split("/")[-1])
     basefile    = file.split("/")[-1]
 
     if os.path.isfile( codefile ):
-        print(f"{col['blue']} [ !!! CACHE: ] {basefile} already exists locally {col['reset']}" )
-        with open("/home/robin/fxgithub/static/code/" + file.split("/")[-1] ) as f:
-            r = f.readlines()
+        logging.warning(f"{col['blue']} [ !!! CACHE: ] {basefile} already exists locally {col['reset']}" )
+        with open( codefile ) as f:
+            r = f.read()
     else:
-        print(f"\033[94m [ !!! FRESH: ] {basefile} doesn't exist locally, downloading from github... {col['reset']}")
+        logging.info(f"\033[94m [ !!! FRESH: ] {basefile} doesn't exist locally, downloading from github... {col['reset']}")
         r = (requests.get(rawfileurl)).text
         with open(codefile, "w+") as f:
             f.write(r)
-    
+
+    try:
+        lines       = (list(request.args.keys())[0]).replace("L","").split("-")
+        logging.info(f"{col['green']} [ >>> SERVING: ] {author}/{repo}@{branch} {file} L{str(lines[0])}-{lines[1]} {col['reset']}")
+    except:
+        lines       = ["1", str(len(str(r).split('\n')))]
+        logging.warning(f"{col['red']} [ !!! ERROR: ] Failed to read line number argument. Setting to length of document. (L{lines[0]}-{lines[1]}) {col['reset']}" )
+
+    imagefile   = ("/home/robin/fxgithub/static/img/" + author + "_" + repo + "_" + branch + "_" + file.split("/")[-1].split(".")[0] + "_" + "L" + lines[0] + "-" + lines[1] + ".png" )
+    githuburl   = ("https://github.com/" + author + "/" + repo + "/" + action + "/" + branch + "/" + file + "#L" + lines[0] + "-" + lines[1])
+
     if os.path.isfile( imagefile ):
-        print( f"{col['blue']} [ !!! CACHE: ] {imagefile} already exists locally {col['reset']}" )
+        logging.warning( f"{col['blue']} [ !!! CACHE: ] {imagefile} already exists locally {col['reset']}" )
     else:
-        print( f"{col['green']} [ !!! FRESH: ] {imagefile} doesn't exist locally, creating image... {col['reset']}")
+        logging.info( f"{col['green']} [ !!! FRESH: ] {imagefile} doesn't exist locally, creating image... {col['reset']}")
         create_image = subprocess.run(["carbon-now", codefile, "--save-to", "/home/robin/fxgithub/static/img", "--save-as", imagefile.split("/")[-1].replace(".png",""), "--headless", "--start", lines[0], "--end", lines[1]])
 
-    print(f"{col['green']} [ >>> SERVING: ] {author}/{repo}@{branch} {file} L{lines[0]}-{lines[1]} {col['reset']}")
+    logging.info(f"{col['green']} [ >>> SERVING: ] {author}/{repo}@{branch} {file} L{lines[0]}-{lines[1]} {col['reset']}")
 
     argstring = (f"Lines {lines[0]}-{lines[1]} of {basefile} from {author}/{repo}@{branch}:\n\n")
     return render_template('default.html', img=("https://fxgithub.com/static/img/" + imagefile.split("/")[-1]), message=argstring, original=githuburl)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.config['SERVER_NAME']='localhost:80'
